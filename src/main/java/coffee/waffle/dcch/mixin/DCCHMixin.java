48ea7a0c929d21692c1e7cd33325b084cb10b0d1
Emma Waffle <emmaffle@modrinth.com>
2021-07-08 14:15:16 -0400

    first commit (1.7.10)

---
+++ src/main/java/coffee/waffle/dcch/mixin/DCCHMixin.java
@@ -0,0 +1,27 @@
+package coffee.waffle.dcch.mixin;
+
+import java.util.List;
+import net.minecraft.client.gui.hud.ChatHudLine;
+import net.minecraft.client.gui.hud.ChatHud;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+/**
+ * @author anar4732
+ */
+@Mixin(ChatHud.class)
+public class DCCHMixin {
+  @Shadow @Final private List<ChatHudLine> field_10040;
+  @Shadow @Final private List<ChatHudLine> field_10039;
+
+  /**
+   * @author anar4732
+   */
+  @Overwrite
+  public void clear() {
+    this.field_10039.clear();
+    this.field_10040.clear();
+  }
+}
